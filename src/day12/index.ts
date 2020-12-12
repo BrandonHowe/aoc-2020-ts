6745c9fc1d74eeb58192013089f94237b6840ecc
xWafl <73sampleperson@gmail.com>
2020-12-12 15:01:13 -0500

    feat: day 12

--- src/day12/index.ts
+++ src/day12/index.ts
@@ -1,12 +1,109 @@
 import * as path from "path";
-import { readInputSplitNum } from "../helpers/readInput";
+import { readInputSplit } from "../helpers/readInput";
+
+const part1 = (arr: [string, number][]) => {
+    const startingPos = [0, 0];
+    let direction = 1;
+    arr.map(l => {
+        switch (l[0]) {
+            case "N":
+                startingPos[1] += l[1];
+                break;
+            case "E":
+                startingPos[0] += l[1];
+                break;
+            case "S":
+                startingPos[1] -= l[1];
+                break;
+            case "W":
+                startingPos[0] -= l[1];
+                break;
+            case "F":
+                switch (direction) {
+                    case 0:
+                        startingPos[1] += l[1];
+                        break;
+                    case 1:
+                        startingPos[0] += l[1];
+                        break;
+                    case 2:
+                        startingPos[1] -= l[1];
+                        break;
+                    case 3:
+                        startingPos[0] -= l[1];
+                        break;
+                }
+                break;
+            case "R":
+                direction = (direction + Math.floor(l[1] / 90)) % 4;
+                break;
+            case "L":
+                direction = (direction + 4 - Math.floor(l[1] / 90)) % 4;
+                break;
+        }
+    });
+    return Math.abs(startingPos[0]) + Math.abs(startingPos[1]);
+};
+
+const part2 = (arr: [string, number][]) => {
+    const waypoint = [10, 1];
+    const startingPos = [0, 0];
+    arr.map(l => {
+        switch (l[0]) {
+            case "N":
+                waypoint[1] += l[1];
+                break;
+            case "E":
+                waypoint[0] += l[1];
+                break;
+            case "S":
+                waypoint[1] -= l[1];
+                break;
+            case "W":
+                waypoint[0] -= l[1];
+                break;
+            case "F":
+                for (let i = 0; i < l[1]; i++) {
+                    startingPos[0] += waypoint[0];
+                    startingPos[1] += waypoint[1];
+                }
+                break;
+            case "R":
+                for (let i = 0; i < l[1] / 90; i++) {
+                    const [nw1, nw0] = waypoint;
+                    waypoint[0] = nw0;
+                    waypoint[1] = nw1 * -1;
+                }
+                break;
+            case "L":
+                for (let i = 0; i < l[1] / 90; i++) {
+                    const [nw1, nw0] = waypoint;
+                    waypoint[0] = nw0 * -1;
+                    waypoint[1] = nw1;
+                }
+                break;
+        }
+    });
+    return Math.abs(startingPos[0]) + Math.abs(startingPos[1]);
+};
 
 const main = async () => {
-    const input = await readInputSplitNum(path.join(__dirname, "./input.txt"));
+    const input: [string, number][] = await (
+        await readInputSplit(path.join(__dirname, "./input.txt"))
+    ).map(l => [l.charAt(0), Number(l.slice(1))]);
+    input.pop();
 
-    console.time("main");
+    console.time("part1");
 
-    console.timeEnd("main");
+    console.log("Part 1:", part1(input));
+
+    console.timeEnd("part1");
+
+    console.time("part2");
+
+    console.log("Part 2:", part2(input));
+
+    console.timeEnd("part2");
 };
 
 main();
